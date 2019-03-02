73763223036a7386ab3897b6b58ff454d68ed732
Mario <quiniasol@hotmail.com>
2019-03-02 12:16:56 +0100

    Added reverse mode

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Reddit Unit Conversor
+// @name         Reddit Unit Converter
 // @version      0.1
 // @description  Converts lbs to kgs
 // @author       Mario O.M.
-// @namespace    https://github.com/marioortizmanero/RedditLbsToKgsConversor
+// @namespace    https://github.com/marioortizmanero/RedditLbsToKgsConverter
 // @match        *://*.reddit.com/*
 // @grant        none
 // ==/UserScript==
@@ -17,27 +17,48 @@
      * Change 'round' below to 'true' if you prefer a double decimal result like '25.83'
      * instead of '26'.
      *
+     * Change 'reverse' to 'true' if you want to convert from kgs to lbs instead.
+     *
     */
     'use strict';
     const lbsToKgsDivision = 2.205;
     const commentClassName = "md";
     const titleClassName = "title";
+    const reverseConverter = false;
     const round = false;
+
+    const lbsToKgRegex = /\d*\s?(lbs|pounds)/g;
+    const kgToLbsRegex = /\d*\s?(kg|kgs|kilograms)/g;
 
     var comments = document.getElementsByClassName(commentClassName);
     var titles = document.getElementsByClassName(titleClassName);
 
     // Modifies the numbers and units
-    function replacer(match, p1, p2, string) {
+    function lbsToKg(match, p1, p2, string) {
         p1 = "kg";
         p2 = round ? Math.round(p2/lbsToKgsDivision*100)/100 : Math.round(p2/lbsToKgsDivision);
-        return [p2, " ", p1].join("");
+        return [p2, p1].join("");
     }
+
+    function kgToLbs(match, p1, p2, string) {
+        p1 = "lbs";
+        p2 = p2*lbsToKgsDivision;
+        return [p2, p1].join("");
+    }
+
     // Regex expressions:
     for (var i=0;i<titles.length; i++) {
-        titles[i].innerHTML = titles[i].innerHTML.replace(/\d*\s?(lbs|pounds)/g, replacer);
+        if(reverseConverter) {
+            titles[i].innerHTML = titles[i].innerHTML.replace(kgToLbsRegex, kgToLbs);
+        } else {
+            titles[i].innerHTML = titles[i].innerHTML.replace(lbsToKgRegex, lbsToKg);
+        }
     }
     for (var j=0;j<comments.length; j++) {
-        comments[j].innerHTML = comments[j].innerHTML.replace(/\d*\s?(lbs|pounds)/g, replacer);
+        if(reverseConverter) {
+            comments[j].innerHTML = comments[j].innerHTML.replace(kgToLbsRegex, kgToLbs);
+        } else {
+            comments[j].innerHTML = comments[j].innerHTML.replace(lbsToKgRegex, lbsToKg);
+        }
     }
 })();
